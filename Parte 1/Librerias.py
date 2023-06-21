818d6fd72670306565fd484fd7f2142a760fdd8c
diegoelizalde4 <diegoelizalde475@gmail.com>
2023-06-21 12:29:24 -0600

    Importacion de el conjunto de datos y un archivo donde se encuentra

---
+++ Parte 1/Librerias.py
@@ -0,0 +1,9 @@
+#Importe de librerias para data science
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importe de dataset csv mediante pandas
+dataset = pd.read_csv('Data.csv') 
+
+
