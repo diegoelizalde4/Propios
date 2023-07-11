2784cfb1ad234c4f9399f1c5da6a2101f23496a6
diegoelizalde4 <diegoelizalde475@gmail.com>
2023-07-11 09:12:55 -0600

    Entrenamiento de modelo

--- Parte 1/Librerias.py
+++
@@ -1,9 +0,0 @@
-#Importe de librerias para data science
-import numpy as np 
-import matplotlib.pyplot as plt
-import pandas as pd
-
-#Importe de dataset csv mediante pandas
-dataset = pd.read_csv('Data.csv') 
-
-
